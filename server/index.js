5dff7758e3b0b13b1c884a6526ed1b700365e0ed
Johnny <dos_24@inbox.ru>
2021-03-08 14:06:09 +0500

    add 2 requests for default stations

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const path = require('path')
 const sequelize = require('./utils/database')
-const stationsRoute = require('./routes/stations')
+const defaultStations = require('./routes/defaultStations')
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.use('/api/stations', stationsRoute)
+app.use('/api/defaultStations', defaultStations)
 
 app.use((req, res, next) => {
 	res.sendFile('/index.html')
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
 
 async function start() {
 	try {
+		// await sequelize.sync({ force: true })
 		await sequelize.sync()
 		app.listen(PORT)
 	} catch(e) {
