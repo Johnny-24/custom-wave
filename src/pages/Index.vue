0f556459696837cfd9d1a2b0eb171a297c34f016
johnny-24 <dos_24@inbox.ru>
2021-04-03 01:00:38 +0500

    init quasar

---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    Main page
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'PageIndex',
+});
+</script>
